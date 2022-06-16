313b53b8377a5cece73325336b0264ca86a3c239
OPunga <ronnyopunga96@gmail.com>
2022-06-16 13:07:08 -0700

    Header

--- monty.h
+++ monty.h
@@ -1,17 +1,21 @@
-#ifndef __MONTY_H__
-#define __MONTY_H__
+#ifndef MONTY_H
+#define MONTY_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#include <ctype.h>
 
-#define STACK 0
-#define QUEUE 1
-#define DELIMS " \n\t\a\b"
-
-/* GLOBAL OPCODE TOKENS */
-extern char **op_toks;
-
+#define UNUSED(x) (void)(x)
+#define TRUE 1
+#define FALSE 0
+#define DELIMS " \n\t\r"
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -29,7 +33,7 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
@@ -42,48 +46,52 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* PRIMARY INTERPRETER FUNCTIONS */
-void free_stack(stack_t **stack);
-int init_stack(stack_t **stack);
-int check_mode(stack_t *stack);
-void free_tokens(void);
-unsigned int token_arr_len(void);
-int run_monty(FILE *script_fd);
-void set_op_tok_error(int error_code);
+/**
+ * struct glob_s - globally useful variables, all rolled into one
+ * @top: double pointer to top of stack
+ * @ops: double pointer to an instruction struct
+**/
+typedef struct glob_s
+{
+	stack_t **top;
+	instruction_t **ops;
+} glob_t;
 
-/* OPCODE FUNCTIONS */
-void monty_push(stack_t **stack, unsigned int line_number);
-void monty_pall(stack_t **stack, unsigned int line_number);
-void monty_pint(stack_t **stack, unsigned int line_number);
-void monty_pop(stack_t **stack, unsigned int line_number);
-void monty_swap(stack_t **stack, unsigned int line_number);
-void monty_add(stack_t **stack, unsigned int line_number);
-void monty_nop(stack_t **stack, unsigned int line_number);
-void monty_sub(stack_t **stack, unsigned int line_number);
-void monty_div(stack_t **stack, unsigned int line_number);
-void monty_mul(stack_t **stack, unsigned int line_number);
-void monty_mod(stack_t **stack, unsigned int line_number);
-void monty_pchar(stack_t **stack, unsigned int line_number);
-void monty_pstr(stack_t **stack, unsigned int line_number);
-void monty_rotl(stack_t **stack, unsigned int line_number);
-void monty_rotr(stack_t **stack, unsigned int line_number);
-void monty_stack(stack_t **stack, unsigned int line_number);
-void monty_queue(stack_t **stack, unsigned int line_number);
+extern glob_t glob;
 
-/* CUSTOM STANDARD LIBRARY FUNCTIONS */
-char **strtow(char *str, char *delims);
-char *get_int(int n);
+/* monty.c */
+void stack_init(stack_t **head);
+void free_all(void);
 
-/* ERROR MESSAGES & ERROR CODES */
-int usage_error(void);
-int malloc_error(void);
-int f_open_error(char *filename);
-int unknown_op_error(char *opcode, unsigned int line_number);
-int no_int_error(unsigned int line_number);
-int pop_error(unsigned int line_number);
-int pint_error(unsigned int line_number);
-int short_stack_error(unsigned int line_number, char *op);
-int div_error(unsigned int line_number);
-int pchar_error(unsigned int line_number, char *message);
+/* helper1.c */
+int process_file(char *filename, stack_t **stack);
 
-#endif /* __MONTY_H__ */
+/* helper2.c */
+void delegate_op(stack_t **stack, char *op, unsigned int line_number);
+
+/* pushPall.c */
+void p_ush(stack_t **stack, unsigned int line_number);
+void p_all(stack_t **stack, unsigned int line_number);
+void p_int(stack_t **stack, unsigned int line_number);
+void p_op(stack_t **stack, unsigned int line_number);
+void s_wap(stack_t **stack, unsigned int line_number);
+
+
+void instruction_add(stack_t **stack, unsigned int line_number);
+void instruction_nop(stack_t **stack, unsigned int line_number);
+void instruction_sub(stack_t **stack, unsigned int line_number);
+void instruction_div(stack_t **stack, unsigned int line_number);
+void instruction_mul(stack_t **stack, unsigned int line_number);
+
+/* instruction3.c */
+void instruction_mod(stack_t **stack, unsigned int line_number);
+void instruction_pchar(stack_t **stack, unsigned int line_number);
+void instruction_pstr(stack_t **stack, unsigned int line_number);
+void instruction_rotl(stack_t **stack, unsigned int line_number);
+void instruction_rotr(stack_t **stack, unsigned int line_number);
+
+/* _strtol.c */
+int is_leading_digit(char ascii_char);
+int _strtol(char *num_string, unsigned int line_number);
+
+#endif
